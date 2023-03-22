1e87405760113bf9971eb3c38656839370c50c42
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-03-22 21:11:28 +0000

    aluno.h agora está comentada

--- aluno.h
+++ aluno.h
@@ -3,10 +3,16 @@
 
 typedef struct aluno Aluno;
 
-void ordenaAlunos(int N, Aluno *Vet);
-
+/* Recebe como parâmetro um número inteiro "N", retornado um vetor de "Aluno". 
+"N" corresponde ao tamanho do vetor de "Aluno". */
 Aluno *AlocaAlunos(int N);
 
+/* Recebe como parâmetro um número inteiro "N" e uma struct "Aluno", não há retorno. 
+Ordena alfabeticamente o nome dos alunos. */
+void ordenaAlunos(int N, Aluno *Vet);
+
+/* Recebe como parâmetro um número inteiro "N" e uma struct "Aluno", não há retorno. 
+Imprime os dados dos alunos no arquivo. */
 void ImprimeAluno(int N, Aluno *Vet);
 
 #endif
