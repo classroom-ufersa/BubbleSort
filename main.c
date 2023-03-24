231c60f1ebfa73fd1d9d7726ba3d565bafc2c0a8
Ana Santana <123104984+Anaasca07@users.noreply.github.com>
2023-03-24 17:20:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,26 +5,26 @@
 
 int main()
 {
-    FILE *arch = fopen("ListaDeAlunos.txt", "w"); // Abre e cria um arquivo no modo "write" escrita
+    FILE *arch = fopen("ListaDeAlunos.txt", "w"); // Abre e cria um arquivo no modo "write" (escrita)
     int n, i; /* Declaração de uma variável que armazena e define a quantidade de pessoas
     que serão cadastradas "n" e uma variavel de contagem "i" */
 
     printf("\nInforme a quantidade de alunos a serem cadastradas: ");
     scanf("%i", &n); // Lendo a quantidade de pessoas que serão cadastradas
-    // Criação de ponteiro do tipo da struct Aluno. Chamada da função que será usada para alocar um vetor de Aluno
+    // Criaçao de ponteiro do tipo da struct Aluno. Chamada da função que será usada para alocar um vetor de Aluno
     // Atribuição do valor que será retornado da função que aloca um vetor para o ponteiro da struct do tipo Aluno
-    Aluno *p = AlocaAlunos(n);
+    Aluno* p= AlocaAlunos(n);
+    Documento* d= AlocaDocumento();
 
-    ordenaAlunos(n, p); // Função do Bubble Sort
-    ImprimeAluno(n, p); // Imprime os dados dos alunos no arquivo
-    
-    for (i = 0; i < n; i++)
-    {
-        fprintf(arch, "Nome: %s\nMatricula: %i\nDocumento: %i\n\n", p[i].nome, p[i].mat, p[i].doc);
+    ordenaAlunos(n, p);
+    ImprimeAluno(n, p);
+    for (i = 0; i < n; i++){
+        fprintf(arch, "Nome: %s\nMatricula: %i\nDocumento: %i\n\n", p[i].nome, p[i].mat, p[i].d.CPF);
     }
 
     fclose(arch);
-    free(p); // Liberando espaço alocado dinamicamente da struct de "Aluno" "p"
+    free(p); // Liberando espaço alocado dinamicamente da struct "Aluno"
+    free(d); // Liberando espaço alocado dinamicamente da union "Documento"
     printf("\nPrograma finalizado!\n");
     system("pause");
     printf("\n\n");
