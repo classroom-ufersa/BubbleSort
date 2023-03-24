cc86a77bd6467a0efb9aa7c70d8f063306c0ea66
Ana Santana <123104984+Anaasca07@users.noreply.github.com>
2023-03-24 17:20:01 -0300

    Update aluno.h

--- aluno.h
+++ aluno.h
@@ -1,18 +1,22 @@
 #ifndef ALUNO_H_INCLUDED
 #define ALUNO_H_INCLUDED
 
+typedef union documento Documento;
+
 typedef struct aluno Aluno;
 
-/* Recebe como parâmetro um número inteiro "N", retornado um vetor de "Aluno". 
+/* Recebe como parametro um número inteiro "N", retornado um vetor de "Aluno". 
 "N" corresponde ao tamanho do vetor de "Aluno". */
 Aluno *AlocaAlunos(int N);
+
+Documento* AlocaDocumento(void); //aloca union "Documento" e recebe void como parâmetro;
 
 /* Recebe como parâmetro um número inteiro "N" e uma struct "Aluno", não há retorno. 
 Ordena alfabeticamente o nome dos alunos. */
 void ordenaAlunos(int N, Aluno *Vet);
 
 /* Recebe como parâmetro um número inteiro "N" e uma struct "Aluno", não há retorno. 
-Imprime os dados dos alunos no arquivo criado. */
+Imprime os dados dos alunos no arquivo. */
 void ImprimeAluno(int N, Aluno *Vet);
 
 #endif
