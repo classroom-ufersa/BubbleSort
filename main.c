a7357cad7192ee2198321a93b4c3261957f140f7
Rafael O. Freire <120592511+rfreir3@users.noreply.github.com>
2023-03-26 19:08:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,23 +3,24 @@
 #include <stdlib.h>
 #include "aluno.c"
 
-int main()
-{
-    FILE *arch = fopen("ListaDeAlunos.txt", "w"); // Abre e cria um arquivo no modo "write" (escrita)
+int main(){
+    FILE *arch = fopen("ListaDeAlunos.txt", "w+"); // Abre e cria um arquivo no modo "write" (escrita)
+    if(arch==NULL){
+        printf("Erro ao abrir arquivo!");
+        exit(1);
+    }
     int n, i; /* Declaração de uma variável que armazena e define a quantidade de pessoas
     que serão cadastradas "n" e uma variavel de contagem "i" */
 
-    printf("\nInforme a quantidade de alunos a serem cadastradas: ");
-    scanf("%i", &n); // Lendo a quantidade de pessoas que serão cadastradas
     // Criaçao de ponteiro do tipo da struct Aluno. Chamada da função que será usada para alocar um vetor de Aluno
     // Atribuição do valor que será retornado da função que aloca um vetor para o ponteiro da struct do tipo Aluno
-    Aluno* p= AlocaAlunos(n);
+    Aluno* p= AlocaAlunos(arch);
     Documento* d= AlocaDocumento();
 
     ordenaAlunos(n, p);
     ImprimeAluno(n, p);
     for (i = 0; i < n; i++){
-        fprintf(arch, "Nome: %s\nMatricula: %i\nDocumento: %i\n\n", p[i].nome, p[i].mat, p[i].d.CPF);
+        fprintf(arch, "%s\n%i\n%i\n\n", p[i].nome, p[i].mat, p[i].d.CPF);
     }
 
     fclose(arch);
