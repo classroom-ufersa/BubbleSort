cc614bcd4ea48a845c349a355ab12a770b3da6d1
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-03-28 17:40:57 +0000

    Atualização Geral

--- main.c
+++ main.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include "aluno.c"
 
-int main(){
-    
-    clock_t inicio, fim; // Variáveis para a contagem do tempo do algoritmo
+int main()
+{
+    clock_t inicio = 0, fim; // Variáveis para a contagem do tempo do algoritmo
     double tempo_execucao; // Variável para guardar o tempo de execução do algoritmo
+    int n; // Variável para armazenagem da quantidade de alunos cadastradas
     
-    FILE *arch = fopen("ListaDeAlunos.txt", "w+"); // Abre e cria um arquivo no modo "write" (escrita)
-    if(arch==NULL){
-        printf("Erro ao abrir arquivo!");
-        exit(1);
+    FILE *arch = fopen("ListaDeAlunos.txt", "w"); // Abre e cria um arquivo no modo "write" (escrita)
+
+    // Análize se o arquivo foi  criado ou não
+    if(arch != NULL){ 
+        printf("\nArquivo criado!\n");
     }
-    int n, i; /* Declaração de uma variável que armazena e define a quantidade de pessoas
-    que serão cadastradas "n" e uma variavel de contagem "i" */
-
-    // Criaçao de ponteiro do tipo da struct Aluno. Chamada da função que será usada para alocar um vetor de Aluno
-    // Atribuição do valor que será retornado da função que aloca um vetor para o ponteiro da struct do tipo Aluno
-    Aluno* p= AlocaAlunos(arch);
-    Documento* d= AlocaDocumento();
-
-    ordenaAlunos(n, p);
-    ImprimeAluno(n, p);
-    for (i = 0; i < n; i++){
-        fprintf(arch, "%s\n%i\n%i\n\n", p[i].nome, p[i].mat, p[i].d.CPF);
+    else if(arch == NULL){
+        printf("\nProblema ao criar o arquivo!\n");
     }
 
-    fclose(arch);
-    free(p); // Liberando espaço alocado dinamicamente da struct "Aluno"
-    free(d); // Liberando espaço alocado dinamicamente da union "Documento"
-    printf("\nPrograma finalizado!\n");
-    system("pause");
-    printf("\n\n");
+    printf("\nInforme a quantidade de alunos a serem cadastradas: ");
+    scanf("%i", &n); // Lendo a quantidade de pessoas que serão cadastradas
     
+    // Criação de um ponteiro "p" e alocação de um vetor struct "Aluno" por meio da função "AlocaAluno"
+    Aluno* p= AlocaAlunos(n);
+
+    OrdenaAlunos(n, p); // Iplementação do Bubble Sort
+    ImprimeAluno(n, p, arch); // Imprime dados dos alunos no terminal e no arquivo criado
+
+    fclose(arch); // Fecha o arquivo criado
+    free(p); // Liberando espaço alocado dinamicamente da struct "Aluno" e consequentemente de "Documentos"
+
     fim = clock(); // "fim" recebe o tempo de execução
     tempo_execucao = (double)(fim - inicio) / CLOCKS_PER_SEC; // "tempo_execucao" recebe o resultado da divisão entre tempo "inicio" e tempo "fim"
-    printf("Tempo de execução: %f segundos\n", tempo_execucao) // Amostragem do tempo de execução
+    printf("\nTempo de execucao: %f segundos\n\n", tempo_execucao); // Amostragem do tempo de execução
         
     return 0;
 }
