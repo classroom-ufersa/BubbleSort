25fef13cd9defae30f2c37ffc49429c4a132f774
Tata <112900054+JanieliSilva@users.noreply.github.com>
2023-03-30 12:37:17 -0300

    Update aluno.c

--- aluno.c
+++ aluno.c
@@ -51,8 +51,8 @@ Complexidade da função "OrdenaAlunos" - Bubble Sort
 
 Pior caso:
 
-T(O) = n + n-1 + c1 + c2 + c3+ c4 + c5
-T(O) = n * (n-1) / 2 + C
+T(O) = n * n-1 + c1 + c2 + c3+ c4 + c5
+T(O) = n * (n-1) + C
 T(O) = n² 
 
 O(n²)
