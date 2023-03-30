420ea31b6dc6eb5d01fd2081d6a1874a8297ef24
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-03-30 12:05:16 -0300

    Atualização: Renomeações

--- aluno.h
+++ aluno.h
@@ -1,11 +1,11 @@
 /* Recebe como parametro um número inteiro "n", retornado um vetor de "Aluno". 
 "n" corresponde ao tamanho do vetor de "Aluno". */
-Aluno *AlocaAlunos(int n);
+Aluno *AlocaAlunos(int numero_alunos);
 
 /* Recebe como parâmetro um número inteiro "n" e uma struct "Aluno", não há retorno. 
 Ordena alfabeticamente o nome dos alunos. */
-void OrdenaAlunos(int n, Aluno *Vet);
+void OrdenaAlunos(int numero_alunos, Aluno *vetor);
 
 /* Recebe como parâmetro um número inteiro "n", uma struct "Aluno" e um arquivo "FILE", não há retorno. 
 Imprime os dados dos alunos no terminal e no arquivo. */
-void ImprimeAluno(int n, Aluno *Vet, FILE *arch);
+void ImprimeAluno(int numero_alunos, Aluno *vetor, FILE *arquivo);
